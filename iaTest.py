0ac4e0de7d2b231c7b182df6a7ef1ddf32cb3384
CandyDelChurros <Cayo.veiga54@gmail.com>
2024-11-12 19:27:48 -0400

    Paint funcionando

---
+++ iaTest.py
@@ -0,0 +1,9 @@
+import PIL.Image
+import google.generativeai as genai
+
+genai.configure(api_key="AIzaSyBq2nc05mUMNpuyQMsW-j69L7Qtl-Chf0A")
+model = genai.GenerativeModel("gemini-1.5-flash")
+letra = PIL.Image.open("img2.png")
+response = model.generate_content(["Qual letra é essa ?", letra])
+print(response.text)
+
