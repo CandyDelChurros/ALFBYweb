d0d183ef6bb574e99914bb7fbcc8d4f055742ee1
CandyDelChurros <Cayo.veiga54@gmail.com>
2024-12-01 17:00:30 -0400

    V1

--- iaTest.py
+++
@@ -1,9 +0,0 @@
-import PIL.Image
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyBq2nc05mUMNpuyQMsW-j69L7Qtl-Chf0A")
-model = genai.GenerativeModel("gemini-1.5-flash")
-letra = PIL.Image.open("letra.png")
-response = model.generate_content(["Qual letra é essa ?", letra])
-print(response.text)
-
